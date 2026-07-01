f330b7c29ffc86d36f29aaaa00a9d07304aaca64
opheliadelrey13 <opheliadelrey13@gmail.com>
2026-07-01 23:03:34 +0300

    Added calculator menu

---
+++ calculator/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        System.out.println("---CALCULATOR---");
+        System.out.println("1-Addition");
+        System.out.println("2-Substraction");
+        System.out.println("3-Multiplication");
+        System.out.println("4-Division");
+        System.out.println("5-Exit");
+        System.out.println();
+        System.out.print("Your choice: ");
+    }
+}
