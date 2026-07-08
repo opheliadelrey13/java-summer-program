cd3588a99fb97809a4eaecffbe7010b26140e5d9
opheliadelrey13 <opheliadelrey13@gmail.com>
2026-07-08 13:57:33 +0300

    Added Library class for managing items.

--- stage-1-oop-library/Library.java
+++ stage-1-oop-library/Library.java
@@ -1,2 +1,57 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+    private List<LibraryItem> items;
+
+    public Library(){
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(LibraryItem item){
+        items.add(item);
+        System.out.println(item.getTitle() + " has been added to the library.");
+    }
+
+    public void listItems() {
+        if (items.isEmpty()){
+            System.out.println("There are no items in the library.");
+        }else{
+            for (LibraryItem item : items){
+                item.printInfo();
+            }
+        }
+    }
+
+    public LibraryItem findById(int id){
+        for (LibraryItem item : items){
+            if (item.getId() == id){
+                item.printInfo();
+            }
+        }
+        return null;
+    }
+
+    public void borrowById(int id){
+        LibraryItem item = findById(id);
+
+        if (item == null){
+            System.out.println("No item found with the ID: " + id);
+        }else{
+            item.borrow();
+        }
+    }
+
+    public void returnItemById(int id){
+        LibraryItem item = findById(id);
+
+        if (item == null){
+            System.out.println("No item found with the ID: " + id);
+        }else{
+            item.returnBorrowed();
+        }
+
+        }
+    }
 }
