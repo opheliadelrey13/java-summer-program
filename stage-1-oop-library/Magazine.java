e1aa8aaf2ff3f5316bd863941a543e365d78b23b
opheliadelrey13 <opheliadelrey13@gmail.com>
2026-07-09 13:06:51 +0300

    Added Magazine class.

--- stage-1-oop-library/Magazine.java
+++ stage-1-oop-library/Magazine.java
@@ -1,2 +1,23 @@
-public class Magazine {
+public class Magazine extends LibraryItem {
+    private int issueNumber;
+
+    public Magazine(int id,String title,int isuueNumber){
+        super(id, title);
+        this.issueNumber = isuueNumber;
+    }
+
+    public int getIssueNumber(){
+        return issueNumber;
+    }
+
+    public void printInfo(){
+        String status = isBorrowed() ? "Borrowed" : "Available";
+        System.out.println("MAGAZINE | ID: " + getId()
+            + " | TITLE: " + getTitle()
+            + " | ISSUE: " + getIssueNumber()
+            + " | STATUS: " + status
+        );
+    }
+
+
 }
