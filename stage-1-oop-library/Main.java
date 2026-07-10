fd64e9cd72965900fa069b9852e7be0e5c7de86a
opheliadelrey13 <opheliadelrey13@gmail.com>
2026-07-11 02:10:01 +0300

    Added Main class and complete library project.

--- stage-1-oop-library/Main.java
+++ stage-1-oop-library/Main.java
@@ -1,39 +1,113 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+
+    private static int nextId = 1;
     static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Library library = new Library();
 
-        Book book1 = new Book(1,"1984","George Orwell",328);
-        Book book2 = new Book(2, "Crime and Punishment", "Dostoevsky", 671);
-        Magazine magazine1 = new Magazine(3,"Science Monthly",45);
+        int choice = 0;
 
-        library.addItem(book1);
-        library.addItem(book2);
-        library.addItem(magazine1);
+        while (choice != 6){
+            menu();
+            choice = readInt(scanner, "Choose an option: ");
 
+            switch (choice) {
+                case 1:
+                    addBook(scanner, library);
+                    break;
+                case 2:
+                    addMagazine(scanner,library);
 
-        System.out.println();
-        library.listItems();
-
-        System.out.println();
-        library.borrowById(1);
-
-        System.out.println();
-        library.listItems();
-
-        System.out.println();
-        library.returnItemById(1);
-
-        System.out.println();
-        library.listItems();
-
-
-
-
-
+                case 3:
+                    library.listItems();
+                    break;
+                case 4:
+                    borrowItem(scanner,library);
+                    break;
+                case 5:
+                    returnItem(scanner,library);
+                    break;
+                case 6:
+                    System.out.println("Exiting from the program...");
+                    break;
+                default:
+                    System.out.println("Invalid option.Please choose between 1 and 6");
+                    
+            }
+            System.out.println();
+        }
+        scanner.close();
 
     }
 
+    public static void addBook(Scanner scanner, Library library) {
+        scanner.nextLine();
 
+        System.out.print("Book title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Author: ");
+        String author = scanner.nextLine();
+
+        int pageCount = readInt(scanner, "Page count: ");
+
+        Book book = new Book(nextId, title, author, pageCount);
+        nextId++;
+
+        library.addItem(book);
+    }
+
+    public static void addMagazine(Scanner scanner, Library library) {
+        scanner.nextLine();
+
+        System.out.print("Magazine title: ");
+        String title = scanner.nextLine();
+
+        int issueNumber = readInt(scanner, "Issue number: ");
+
+        Magazine magazine = new Magazine(nextId, title, issueNumber);
+        nextId++;
+
+        library.addItem(magazine);
+    }
+
+    public static void borrowItem(Scanner scanner, Library library) {
+        int id = readInt(scanner, "Enter item ID to borrow: ");
+        library.borrowById(id);
+    }
+
+    public static void returnItem(Scanner scanner, Library library) {
+        int id = readInt(scanner, "Enter item ID to return: ");
+        library.returnItemById(id);
+    }
+
+    private static int readInt(Scanner scanner,String massage) {
+        while (true){
+            try{
+                System.out.print(massage);
+                return scanner.nextInt();
+            }catch(InputMismatchException e) {
+                System.out.println("Invalid input.Please enter an integer.");
+                scanner.nextLine();
+            }
+        }
+
+    }
+
+    private static void menu() {
+
+        System.out.println("----- LIBRARY MANAGEMENT SYSTEM -----");
+        System.out.println("1- Add book");
+        System.out.println("2- Add magazine");
+        System.out.println("3- List items");
+        System.out.println("4- Borrow item");
+        System.out.println("5- Return item");
+        System.out.println("6- Exit");
+
+    }
 
 
 }
